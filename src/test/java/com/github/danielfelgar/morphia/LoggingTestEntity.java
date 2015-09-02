5d958544c2f74e933c0805fec42418e3cc7742c6
danielfelgar <danielsrf_0203@hotmail.com>
2015-09-02 17:27:52 -0300

    Change package

---
+++ src/test/java/com/github/danielfelgar/morphia/LoggingTestEntity.java
@@ -0,0 +1,11 @@
+package com.github.danielfelgar.morphia;
+
+import org.bson.types.ObjectId;
+import org.mongodb.morphia.annotations.Id;
+
+public class LoggingTestEntity {
+    @Id
+    private ObjectId id;
+
+    private int i = 5;
+}
